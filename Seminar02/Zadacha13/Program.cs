d21b6403f00ecccd3a19d3722bc1613e35e12670
Kirill Golovlev <k-ork-a@mail.ru>
2022-12-04 16:12:44 +0300

    Reshil Zadachu 13

---
+++ Seminar02/Zadacha13/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+
+
+
+if (num<0)
+{
+    num=num*(-1);
+}
+
+if (num>999)
+    {
+        while(num>999)
+        {
+            num=num/10;
+        }
+    }
+
+if (num>99)
+{
+    if (num<1000)
+    {
+      num = num%10;
+      
+      Console.WriteLine($"Третья цифра числа: {num}");  
+    }
+    
+  
+
+} 
+else
+{
+    Console.WriteLine("Тертьей цифры нет.");
+}
+
+
+
