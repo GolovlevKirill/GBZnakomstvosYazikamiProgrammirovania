b8ed5f909e63bdfd3f7297813495fab0dc7ad932
Kirill Golovlev <k-ork-a@mail.ru>
2022-12-04 16:31:49 +0300

    Reshil 15-yu zadachu

---
+++ Seminar02/Zadacha15/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите день недели цифрой:");
+string dayNumber = Console.ReadLine();
+int dayNum=Convert.ToInt32(dayNumber);
+
+if (dayNum<1){
+    Console.WriteLine("Вы ввели недопустимое значение. Введите (от 1 до 7)");
+}
+
+if (dayNum>0)
+{
+    if (dayNum<8)
+    {
+        if (dayNum<6){
+            Console.WriteLine($"НЕ выходной");
+        }
+
+        if (dayNum>5){
+            Console.WriteLine("Выходной");
+        }
+    } else 
+    {
+        Console.WriteLine("Вы ввели число больше 7");
+    }
+}
+
+
