56fb7e706db7462f9215dd14e2288b37c0f8aba1
Kirill Golovlev <k-ork-a@mail.ru>
2022-12-10 17:40:29 +0300

    Reshil 21 i 23 zadachi

---
+++ Seminar03/Zadacha21/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+//
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты двух точек в 3D пространстве(X,Y,Z)");
+Console.WriteLine("Координата X первой точки:");
+string X1 = Console.ReadLine();
+int x1 = Convert.ToInt32(X1);
+Console.WriteLine("Координата Y первой точки:");
+string Y1 = Console.ReadLine();
+int y1 = Convert.ToInt32(Y1);
+Console.WriteLine("Координата Z первой точки:");
+string Z1 = Console.ReadLine();
+int z1 = Convert.ToInt32(Z1);
+
+Console.WriteLine("Координата X второй точки:");
+string X2 = Console.ReadLine();
+int x2 = Convert.ToInt32(X2);
+Console.WriteLine("Координата Y второй точки:");
+string Y2 = Console.ReadLine();
+int y2 = Convert.ToInt32(Y2);
+Console.WriteLine("Координата Z второй точки:");
+string Z2 = Console.ReadLine();
+int z2 = Convert.ToInt32(Z2);
+
+Console.WriteLine($"Вы ввели координаты точек A({x1}, {y1}, {z1}) B({x2}, {y2}, {z2})");
+
+// Вычесление расстояние между точками:
+
+double distance= Math.Sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))+((z2-z1)*(z2-z1)));
+
+Console.WriteLine($"Расстояние между точками в 3D пространстве: {distance}");
+
+//int number = Convert.ToInt32(Number);
+// Когда string до конвертации почему не могут быть 
+// переменные с одним и тем же именем?
---
+++ Seminar03/Zadacha23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число:");
+string n = Console.ReadLine();
+int N = Convert.ToInt32(n);
+
+void cub()
+{
+    
+    int step = 1;
+    while (step <= N)
+    {
+        int result = step * step * step;
+        
+        Console.WriteLine($"{result}");
+        step = step + 1;
+    }
+    
+}
+cub();
