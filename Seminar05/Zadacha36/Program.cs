826af8ecc50c30c724ea4c4c430affba0416b27c
Kirill Golovlev <k-ork-a@mail.ru>
2022-12-20 09:27:12 +0300

    Reshil 36-yu zadachu

--- Seminar05/Zadacha36/Program.cs
+++ Seminar05/Zadacha36/Program.cs
@@ -4,3 +4,30 @@
 // [3, 7, 23, 12] -> 19
 //
 // [-4, -6, 89, 6] -> 0
+
+int[] array = new int[5];
+int i = 0;
+
+// Заполнение массива
+System.Console.WriteLine("Заполнение массива:");
+Random rnd = new Random();
+for (i = 0; i < 5; i = i+1)
+    {
+        array[i]=rnd.Next(1,999);
+        System.Console.WriteLine(array[i]);
+
+    
+    }
+
+// Нахождение суммы эллеменитов
+System.Console.WriteLine("Эллементы на нечетных позициях массива:");
+
+int j = 0;
+int result = 0;
+for (j = 0; j < 5; j = j + 2)
+        {
+            result = result + array[j];
+            System.Console.WriteLine(array[j]);
+        }
+
+Console.WriteLine("Сумма эллементов на нечетных позициях равна: "+result);
